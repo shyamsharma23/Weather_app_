5988717ac03de7fb85fa01b58c4c703029d11609
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2022-03-16 11:53:28 -0500

    Commit

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ form.addEventListener("submit",  async (e)=>{
     document.getElementById("text").textContent = `${res.data.current.temp_c} C`
     document.getElementById("text-two").textContent = `${res.data.current.temp_f} F`
     document.getElementById("img").innerHTML = `<img src = "${res.data.current.condition.icon}"/>`
+    if (res.data.current.temp_c >= 5)
+    {
+        document.getElementById("background").classList.remove("background-two")
+        document.getElementById("background").classList.add("background-one")
+        document.getElementById("attribute").textContent = "Photo by Chris Nemeth on Unsplash"
+    }
+    else if(res.data.current.temp_c < 5){
+        document.getElementById("background").classList.remove("background-one")
+        document.getElementById("background").classList.add("background-two")
+        document.getElementById("attribute").textContent = "Photo by Juha Lakaniemi on Unsplash"
+    }
 })
 
 
